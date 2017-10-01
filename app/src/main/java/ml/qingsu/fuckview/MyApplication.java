e2744fe8421240f8e08006138cdeaff319dd216d
YanLu <caoqinmiao@gmail.com>
2017-10-01 13:21:19 +0800

    加入Fabric统计...

--- app/src/main/java/ml/qingsu/fuckview/MyApplication.java
+++ app/src/main/java/ml/qingsu/fuckview/MyApplication.java
@@ -3,6 +3,10 @@ package ml.qingsu.fuckview;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 
 /**
  * Created by w568w on 2017-6-30.
@@ -16,5 +20,6 @@ public class MyApplication extends Application {
         super.onCreate();
         //FIR.init(this);
         con = this;
+        Fabric.with(this, new Crashlytics());
     }
 }
